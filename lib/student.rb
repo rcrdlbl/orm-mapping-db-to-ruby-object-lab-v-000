20492134073be63dfbb6d90843059a52e89f7bf8
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:20:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,13 @@ class Student
   end
 
   def all_students_in_grade_9
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sq)
   end
 
   def save
