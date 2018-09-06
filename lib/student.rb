0a2f4bc535522bdba7ffd55ee441af5849906a84
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:27:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,18 @@ class Student
     end
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
