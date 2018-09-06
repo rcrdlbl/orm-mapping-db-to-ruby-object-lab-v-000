feeb8e76d6e9e257ec5b02d8c6aeb70b79560196
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:27:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
       WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
