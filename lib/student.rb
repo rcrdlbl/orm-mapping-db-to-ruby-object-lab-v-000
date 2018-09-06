94885ba6bc0058d5dea8a5fa54a480ee4284471e
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:28:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,17 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(amount)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, amount).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
