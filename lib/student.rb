e0abe675f022fb644b7657acbf5e4c9f20d74ae9
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:29:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,6 +91,10 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1).first
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
